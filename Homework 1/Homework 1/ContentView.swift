46944b23fbd645e18f9c3747bf2ca66d628dbe1e
sarah almutari <sara0moha78@gmail.com>
2022-03-09 16:45:15 +0300

    done

--- Homework 1/Homework 1/ContentView.swift
+++ Homework 1/Homework 1/ContentView.swift
@@ -9,11 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("MY FAVOURITE MOVIES")
+                .font(.body)
+                .fontWeight(.bold)
+                .padding()
+            Spacer()
+            ZStack{
+                Image("bg")
+                    .resizable()
+    VStack{
+     HStack{
+        Image("download-5")
+        .resizable().frame(width: 130, height: 130)
+         Text("   AVENGERS:ENDGAME    ")
+             .foregroundColor(Color.white)
+         Text("10/10 ")
+         .foregroundColor(Color.white) }
+    HStack{
+        Image("download-2")
+    .resizable().frame(width: 130, height: 130)
+        Text(" HOWL'S MOVING CASTLE")
+            .foregroundColor(Color.white)
+        Text("             10/10  ")
+            .foregroundColor(Color.white)
+                    }
+    HStack{
+    Image("download-3")
+    .resizable().frame(width: 130, height: 130)
+        Text("          SPIRITED AWAY         ")
+            .foregroundColor(Color.white)
+        Text("10/10  ")
+            .foregroundColor(Color.white)
+                    }
+    HStack{
+    Image("download")
+    .resizable().frame(width: 130, height: 130)
+        Text("       THOR RAGNAROK       ")
+            .foregroundColor(Color.white)
+        Text(" 10/10  ")
+            .foregroundColor(Color.white)
+                    }
+        HStack{
+        Image("download-4")
+        .resizable().frame(width: 130, height: 130)
+            Text("                 SEVEN                ")
+                .foregroundColor(Color.white)
+            Text("10/10  ")
+                .foregroundColor(Color.white)
+    }
+            }
+        
     }
 }
-
+}
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
